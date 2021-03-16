2c54807d7617be78cd63846a7532fa04f3988c55
Gerarvvdor <00053520@uca.edu.sv>
2021-03-16 14:14:07 -0600

    First modification

--- Program.cs
+++ Program.cs
@@ -14,11 +14,11 @@ namespace example1
             Console.WriteLine("Enter a value for b:");
             b = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)} ");
+            Console.WriteLine($"Adding {a} - {b} equals {AddNumbers(a, b)} ");
         }
         public static int AddNumbers(int a, int b)
         {
-            return a + b; 
+            return a - b; 
         }
     }
     
